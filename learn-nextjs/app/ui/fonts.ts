fd521f6606d216f40ea1f431ffb29c9ccf08fcc4
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-12-12 07:47:33 +0000

    feat: Switched font import in acme-logo.tsx
    Switched font import from `@/app/ui/fonts` to `./fonts`

--- learn-nextjs/app/ui/fonts.ts
+++ learn-nextjs/app/ui/fonts.ts
@@ -2,3 +2,4 @@ import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const p = Inter({ subsets: ['vietnamese'],weight: ['400', '700'], });
+// const lusitana = Inter({ subsets: ['lusitana'] });
