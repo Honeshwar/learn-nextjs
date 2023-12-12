2699403e6d79d31d1698187e8789ec63bbbd9c98
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-12-12 07:35:51 +0000

    chapter: Navigation between pages

--- learn-nextjs/app/page.tsx
+++ learn-nextjs/app/page.tsx
@@ -36,7 +36,7 @@ export default function Page() {
         alt="Screenshots of the dashboard project showing desktop version"
       />
       <Image
-        src="/hero-mobile.png"
+        src="https://cdn-icons-png.flaticon.com/128/2365/2365487.png"
         width={560}
         height={620}
         className="block md:hidden"
