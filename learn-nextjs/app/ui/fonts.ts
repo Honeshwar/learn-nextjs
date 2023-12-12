32dcdcd1b91f08991cd9a51526094276c2d4008b
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-12-12 08:00:29 +0000

    feat: Export lusitana variable in fonts.ts
    Exported the lusitana variable as a constant in the fonts.ts file.

--- learn-nextjs/app/ui/fonts.ts
+++ learn-nextjs/app/ui/fonts.ts
@@ -2,4 +2,4 @@ import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const p = Inter({ subsets: ['vietnamese'],weight: ['400', '700'], });
-const lusitana = Inter({ subsets: ['vietnamese'] });
+export const lusitana = Inter({ subsets: ['vietnamese'] });
