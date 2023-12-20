6ef6d1ab16b867b1e83f37ae29fa195fb3c677aa
Honeshwar <73346917+Honeshwar@users.noreply.github.com>
2023-12-20 09:59:01 +0000

    feat: Add logout functionality

---
+++ auth-app/src/app/(backend)/api/users/logout/route.ts
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    try {
+        const response = NextResponse.json({ message: "Logout Successfull",success:true }, { status: 200 });
+        response.cookies.set("token","",{httpOnly:true,expires:new Date(0)});
+        return response;
+    } catch (error) {
+        return NextResponse.json({ message: "Something went wrong on Server" }, { status: 500 });
+    }
+}
--- auth-app/src/app/(frontend)/profile/page.tsx
+++ auth-app/src/app/(frontend)/profile/page.tsx
@@ -1,9 +1,40 @@
 "use client"
 
+import axios from "axios"
+import Link from "next/link"
+import { useRouter } from 'next/navigation';
+import React from "react";
+import { Toaster } from 'react-hot-toast';
+import toast from 'react-hot-toast';
+
 export default function ProfilePage() {
+  const [loading,setLoading] = React.useState(false);
+  const router = useRouter();
+ const logout = async() => {
+   try {
+    setLoading(true)
+      const response = await axios.get("/api/users/logout");
+    if(response.data.success){
+      toast.success(response.data.message)
+      return router.push("/login")
+    }
+    throw new Error(response.data.message)
+   } catch (error:any) {
+    console.log(error)
+    toast.error(error)
+   }finally{
+    setLoading(false)
+   }
+
+ }
   return (
+    <>
+    <Toaster position='top-center' reverseOrder={false}/>
     <div>
+      {loading && <p>Loading...</p>}
       <h1>Profile</h1>
+      <button type="button" onClick={logout} className={`p-2 rounded text-white ${loading ? "bg-blue-200" : "bg-blue-500 "}`} disabled={loading}>Logout</button>
     </div>
+    </>
   )
 }
